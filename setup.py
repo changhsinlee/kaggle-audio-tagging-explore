a1cc3b2ff3360047ee2158a855763b462d0f675d
changhsinlee <casper5866@gmail.com>
2018-06-10 21:35:05 -0400

    First commit, project cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Chang's personal repo for Kaggle audio tagging code',
+    author='SummitML',
+    license='MIT',
+)
